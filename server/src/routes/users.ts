d6e65219105124459e7a84515b9c3c4b6874e49f
Fedeya <elfedeminaya@gmail.com>
2020-04-10 17:58:52 -0300

    [Feat] used the auth middleware

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -3,12 +3,14 @@ import { getUsers, createUser, deleteUser, getUser, updateUser } from '../contro
 import { check } from 'express-validator';
 
 import multer from '../lib/multer';
+import auth from '../middlewares/auth';
 
 const router = Router();
 
 // api/users
 router.route('/')
-  .get(getUsers)
+  .get(auth, getUsers)
+  
   .post(multer.single('avatar'), [
     check('name', 'the name is required').not().isEmpty(),
     check('email', 'enter a valid email').isEmail(),
@@ -17,13 +19,20 @@ router.route('/')
   ], createUser);
 
 router.route('/:id')
-  .get(getUser)
-  .put(multer.single('avatar'), [
+  .get(auth,[
+    check('id', 'must be a mongodb valid id').isMongoId()
+  ], getUser)
+
+  .put(auth, multer.single('avatar'), [
+    check('id', 'must be a mongodb valid id').isMongoId(),
     check('name', 'the name is required').not().isEmpty(),
     check('email', 'enter a valid email').isEmail(),
     check('sex', 'enter a valid sex').isIn(['male', 'female']),
     check('password', 'the password at least 6 characters').isLength({ min: 6 })
   ], updateUser)
-  .delete(deleteUser);
+
+  .delete(auth, [
+    check('id', 'must be a mongodb valid id').isMongoId()
+  ], deleteUser);
 
 export default router;
