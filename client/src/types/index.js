a7de2083938015bea0c9f7fd6968af19761710fc
Fedeya <elfedeminaya@gmail.com>
2020-04-12 19:40:43 -0300

    [Feat] added registry types

--- client/src/types/index.js
+++ client/src/types/index.js
@@ -1,3 +1,6 @@
 export const LOGIN_AUTH = 'LOGIN_AUTH';
 export const LOGIN_ERROR = 'LOGIN_ERROR';
+export const REGISTRY_AUTH = 'REGRISTRY_AUTH';
+export const REGISTRY_ERROR = 'REGISTRY_ERROR';
+
 export const GET_USER = 'GET_USER';
