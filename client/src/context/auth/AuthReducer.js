0a89228c046810d9d40763a3532de03f68cbf02b
Fedeya <elfedeminaya@gmail.com>
2020-04-12 18:59:09 -0300

    [Feat] created actions with login and get user connected

--- client/src/context/auth/AuthReducer.js
+++ client/src/context/auth/AuthReducer.js
@@ -1,5 +1,31 @@
+import { GET_USER, LOGIN_AUTH, LOGIN_ERROR } from '../../types';
+
 export default (state, action) => {
   switch(action.type) {
+    case LOGIN_AUTH:
+      localStorage.setItem('token', action.payload);
+      return {
+        ...state,
+        token: action.payload,
+        auth: true
+      }
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+        auth: true,
+        loading: false
+      }
+    case LOGIN_ERROR:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        error: action.payload,
+        auth: null,
+        user: null,
+        token: null,
+        loading: false
+      }
     default:
       return state;
   }
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,21 +1,69 @@
 import React, { useReducer } from 'react';
 
+import axiosClient from '../../config/axios';
+import tokenAuth from '../../config/tokenAuth';
+
 import AuthContext from './AuthContext';
 import AuthReducer from './AuthReducer';
 
+import { LOGIN_AUTH, LOGIN_ERROR, GET_USER } from '../../types';
+
 function AuthState({ children }) {
   const initialState = {
+    token: localStorage.getItem('token'),
     auth: false,
-    user: null
+    user: null,
+    error: null,
+    loading: true
   };
 
   const [state, dispatch] = useReducer(AuthReducer, initialState);
+
+  const userAuth = async () =>  {
+    const token = localStorage.getItem('token');
+    if(token) {
+      tokenAuth(token);
+    }
+
+    try {
+      const res = await axiosClient.get('/auth');
+      dispatch({
+        type: GET_USER,
+        payload: res.data
+      })
+    } catch(err) {
+      dispatch({
+        type: LOGIN_ERROR,
+        paylaod: err.response.data.errors ? err.response.data.errors[0].msg : err.response.data.msg
+      });
+    }
+  }
   
+  const loginAuth = async user => {
+    try {
+      const res = await axiosClient.post('/auth', user);
+
+      dispatch({
+        type: LOGIN_AUTH,
+        payload: res.data
+      })
+
+      userAuth();
+    } catch(err) {
+      dispatch({
+        type: LOGIN_ERROR,
+        payload: err.response.data.errors ? err.response.data.errors[0].msg : err.response.data.msg
+      });
+    }
+  }
+
   return (
     <AuthContext.Provider
-      value={
-        ...state
-      }
+      value={{
+        ...state,
+        loginAuth,
+        userAuth
+      }}
     >
       {children}
     </AuthContext.Provider>
