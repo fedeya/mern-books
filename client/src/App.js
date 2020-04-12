236d993ca745d0fee1c971e4c07364363e1c8e21
Fedeya <elfedeminaya@gmail.com>
2020-04-12 16:35:58 -0300

    [Feat] added books view in router

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // views
 import Login from './components/auth/Login';
 import Registry from './components/auth/Registry';
+import Books from './components/book/Books';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route component={Login} path="/" exact /> 
         <Route component={Registry} path="/register" />
+        <Route component={Books} path="/books" />
       </Switch>
     </Router>
   );
