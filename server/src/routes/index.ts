418fe32ec5f2d8623855bf8f69fb207ee09bbf8a
Fedeya <elfedeminaya@gmail.com>
2020-04-11 23:36:58 -0300

    [Feat] added auth middleware in all books route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 
+
 import users from './users';
 import auth from './auth';
 import books from './books';
+
+import authMiddleware from '../middlewares/auth';
 
 const router = Router();
 
 // routes
 router.use('/users', users);
 router.use('/auth', auth);
-router.use('/books', books);
+router.use('/books', authMiddleware, books);
 
 export default router;
