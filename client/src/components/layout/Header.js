26f86a3a480ca2c7dd34ad96734b287b3d385a79
Fedeya <elfedeminaya@gmail.com>
2020-04-12 18:59:36 -0300

    [Feat] added profile and books link in navbar

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <nav className="bg-blue-500 px-4 py-2 flex">
-      <Link className="text-2xl text-white">
+    <nav className="bg-blue-600 px-4 py-2 flex">
+      <Link to="/books" className="text-2xl font-bold text-white">
         Mern Books
       </Link>
+
+      <ul className="flex text-gray-200 justify-center items-center ml-auto">
+        <li className="mr-2" >
+          <NavLink to="/books" activeClassName="text-white" >
+            Books
+          </NavLink>
+        </li>
+        <li>
+          <NavLink  to="/profile" activeClassName="text-white">
+            Profile
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   </header>
 );
