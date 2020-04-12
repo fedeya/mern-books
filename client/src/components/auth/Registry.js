8a4da117207ccbe4c532e03472ce9b6a005f332e
Fedeya <elfedeminaya@gmail.com>
2020-04-12 03:00:55 -0300

    [Feat] reforge registry component view

--- client/src/components/auth/Registry.js
+++ client/src/components/auth/Registry.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Background from '../../assets/images/books-pile.jpg';
 
 function Registry() {
 
@@ -9,10 +10,10 @@ function Registry() {
   const [sex, setSex] = useState('');
 
   return (
-    <div className="bg-gray-800 w-screen h-screen">
-      <div className="container h-screen flex justify-center items-center bg-gray-800">
-        <div className="bg-white flex flex-col h-auto rounded md:w-1/3 px-6 pt-6 pb-8">
-          <h1 className="text-3xl text-center mb-3">Login</h1>
+    <div className="w-screen h-screen bg-cover" style={{ backgroundImage: `url(${Background})` }} >
+      <div className="container mx-auto h-screen z-10 flex justify-center items-center bg-transparent">
+        <form className="bg-white flex flex-col h-auto rounded w-full lg:w-1/3 px-6 pt-6 pb-8">
+          <h1 className="text-3xl text-center mb-3">Sign Up</h1>
           <input
             type="text"
             className="focus:bg-white w-full py-2 px-3 bg-gray-200 border-2 border-gray-200 focus:border-blue-400 rounded focus:outline-none mb-3"
@@ -28,20 +29,20 @@ function Registry() {
             onChange={e => setEmail(e.target.value)}
           />
           <label className="bg-gray-200 rounded px-3 py-2 mb-3 cursor-pointer text-gray-600">
-            <span>Select Image</span>
-            <input 
-              type="file" 
+            <span>{ avatar === '' ? 'Select Image' : avatar.split('\\')[avatar.split('\\').length - 1] }</span>
+            <input
+              type="file"
               className="hidden"
               value={avatar}
               onChange={e => setAvatar(e.target.value)}
             />
           </label>
           <select
-            className="mb-3"
+            className="mb-3 bg-gray-200 rounded px-3 py-2"
             value={sex}
             onChange={e => setSex(e.target.value)}
           >
-            <option value="">-- Select Gender --</option>
+            <option value="">Select Gender</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
           </select>
@@ -55,9 +56,9 @@ function Registry() {
           <button
             className="bg-blue-500 border-b-4 border-blue-700 hover:bg-blue-400 w-20 font-bold rounded text-white px-3 py-2"
           >
-            Login
+            Sign Up
           </button>
-        </div>
+        </form>
       </div>
     </div>
   );
