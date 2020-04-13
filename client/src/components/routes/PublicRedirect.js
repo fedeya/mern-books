16bc183649f7b51172b48df707e135a036842d00
Fedeya <elfedeminaya@gmail.com>
2020-04-12 21:53:55 -0300

    [Feat] added ReactLoading

--- client/src/components/routes/PublicRedirect.js
+++ client/src/components/routes/PublicRedirect.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { Redirect, Route } from 'react-router-dom';
+import ReactLoading from 'react-loading';
 
 import AuthContext from '../../context/auth/AuthContext';
 
@@ -12,7 +13,11 @@ function PublicRedirect({component: Component, ...props}) {
     // eslint-disable-next-line
   }, []);
 
-  if(!auth && loading) return <p>Loading...</p>
+  if(!auth && loading) return (
+    <div className="h-screen w-screen bg-gray-800 flex items-center justify-center">
+      <ReactLoading width="100px" type="spin" />
+    </div>
+  )
 
   return !auth ? <Route component={Component} {...props} /> : <Redirect to="/books" />
 }
