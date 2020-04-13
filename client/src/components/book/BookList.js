f43ee2e4a01da3d4bd4b46521541c83a683daf57
Fedeya <elfedeminaya@gmail.com>
2020-04-12 22:07:35 -0300

    [Feat] comprobation if not exist books created's

--- client/src/components/book/BookList.js
+++ client/src/components/book/BookList.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import ReactLoading from 'react-loading';
 
 import BookContext from '../../context/book/BookContext';
@@ -18,6 +19,17 @@ function BookList() {
     return (
       <div className="h-screen w-screen flex items-center justify-center">
         <ReactLoading type="spin" width="100" />
+      </div>
+    )
+  }
+
+  if(books.length === 0 && !loading) {
+    return (
+      <div className="text-center text-white text-2xl">
+        <p>No there are books</p>
+        <Link className="text-blue-300" to="/add">
+          Create one
+        </Link>
       </div>
     )
   }
