d965273cc64d9d187b654b88657e79cdeac063e5
Fedeya <elfedeminaya@gmail.com>
2020-04-12 21:18:01 -0300

    [Feat] delete and get all books actions

--- client/src/context/book/BookReducer.js
+++ client/src/context/book/BookReducer.js
@@ -1,6 +1,12 @@
+import { GET_BOOKS } from '../../types';
 
 export default (state, action) => {
   switch(action.type) {
+    case GET_BOOKS:
+      return {
+        ...state,
+        books: action.payload
+      }
     default:
       return state;
   }
--- client/src/context/book/BookState.js
+++ client/src/context/book/BookState.js
@@ -3,18 +3,53 @@ import React, { useReducer } from 'react';
 import BookContext from './BookContext';
 import BookReducer from './BookReducer';
 
+import { GET_BOOKS } from '../../types';
+
+import axiosClient from '../../config/axios';
+
 function BookState({ children }) {
   const initialState = {
-    books: []
+    books: [],
+    book: null,
+    error: null
   };
 
   const [state, dispatch] = useReducer(BookReducer, initialState);
   
+  const getBooks = async () => {
+    try {
+      const res = await axiosClient.get('/books');
+
+      dispatch({
+        type: GET_BOOKS,
+        payload: res.data
+      });
+    } catch(err) {
+      console.log(err.response);
+    }
+  }
+
+  const deleteBook = async id => {
+    try {
+      await axiosClient.delete(`/books/${id}`);
+      const res = await axiosClient.get('/books');
+      
+      dispatch({
+        type: GET_BOOKS,
+        payload: res.data
+      });
+    } catch(err) {
+      console.log(err.response);
+    }
+  }
+
   return (
     <BookContext.Provider
-      value={
-        ...state
-      }
+      value={{
+        ...state,
+        getBooks,
+        deleteBook
+      }}
     >
       {children}
     </BookContext.Provider>
