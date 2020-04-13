9fbdb0b53a828895e8697e46d97d9112e2b6ec7f
Fedeya <elfedeminaya@gmail.com>
2020-04-12 21:54:33 -0300

    [Feat] added react loading when book si deleted or books is not charged

--- client/src/components/book/BookList.js
+++ client/src/components/book/BookList.js
@@ -1,17 +1,26 @@
 import React, { useContext, useEffect } from 'react';
+import ReactLoading from 'react-loading';
 
 import BookContext from '../../context/book/BookContext';
 
 import Book from './Book';
 
 function BookList() {
-  
-  const { books, getBooks } = useContext(BookContext);
+
+  const { books, getBooks, loading } = useContext(BookContext);
 
   useEffect(() => {
     getBooks();
     // eslint-disable-next-line
   }, [])
+
+  if (loading) {
+    return (
+      <div className="h-screen w-screen flex items-center justify-center">
+        <ReactLoading type="spin" width="100" />
+      </div>
+    )
+  }
 
   return (
     <div className="container mt-3 flex flex-wrap mx-auto">
