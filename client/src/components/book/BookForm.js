4370bd18af4a1c1039609eff77db6ccf65539403
Fedeya <elfedeminaya@gmail.com>
2020-04-12 22:15:25 -0300

    [Feat] created BookForm /add route

---
+++ client/src/components/book/BookForm.js
@@ -0,0 +1,15 @@
+import React, { useState, useRef, useContext } from 'react';
+
+import Header from '../layout/Header';
+
+function BookForm() {
+
+  return (
+    <div className="h-screen w-screen bg-gray-800">
+      <Header />
+    </div>
+  );
+
+}
+
+export default BookForm;
