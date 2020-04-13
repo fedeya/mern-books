93c412231712e912b1c829c7a75298edd5a4a3d5
Fedeya <elfedeminaya@gmail.com>
2020-04-12 21:45:01 -0300

    [Feat] added new types for delete create and edit books

--- client/src/types/index.js
+++ client/src/types/index.js
@@ -4,3 +4,9 @@ export const REGISTRY_AUTH = 'REGRISTRY_AUTH';
 export const REGISTRY_ERROR = 'REGISTRY_ERROR';
 
 export const GET_USER = 'GET_USER';
+
+export const GET_BOOKS = 'GET_BOOKS';
+export const ADD_BOOK = 'ADD_BOOK';
+export const DELETE_BOOK = 'DELETE_BOOK';
+export const UPDATE_BOOK = 'UPDATE_BOOK';
+export const ERROR_BOOK = 'ERROR_BOOK';
