ce98a4d224cf98b628fc3969872dec6bf5cfa41a
Fedeya <elfedeminaya@gmail.com>
2020-04-12 21:46:17 -0300

    [Feat] added auth state, book state and tokenAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 
 // views
 import Login from './components/auth/Login';
 import Registry from './components/auth/Registry';
 import Books from './components/book/Books';
+import Profile from './components/user/Profile';
+
+// context
+import AuthState from './context/auth/AuthState';
+import BookState from './context/book/BookState';
+// routes
+import PrivateRoute from './components/routes/PrivateRoute';
+import PublicRedirect from './components/routes/PublicRedirect';
+
+// add token to headers
+import tokenAuth from './config/tokenAuth';
+
+const token = localStorage.getItem('token');
+if (token) tokenAuth(token);
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route component={Login} path="/" exact /> 
-        <Route component={Registry} path="/register" />
-        <Route component={Books} path="/books" />
-      </Switch>
-    </Router>
+    <AuthState>
+      <BookState>
+        <Router>
+          <Switch>
+            <PublicRedirect component={Login} path="/" exact />
+            <PublicRedirect component={Registry} path="/register" />
+            <PrivateRoute component={Books} path="/books" />
+            <PrivateRoute component={Profile} path="/profile" />
+          </Switch>
+        </Router>
+      </BookState>
+    </AuthState>
   );
 }
 
